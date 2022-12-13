c440458104a3b2f2c149314ef292b269c28f0767
Majd Koshakji <koshakji@gmail.com>
2022-12-13 14:40:50 +0100

    Publicized the converters

--- Sources/Predicates.swift/Converters/Key.swift
+++ Sources/Predicates.swift/Converters/Key.swift
@@ -5,20 +5,20 @@
 //  Created by Majd Koshakji on 14/11/22.
 //
 
-protocol Key {
+public protocol Key {
     var stringValue: String { get }
 }
 
 extension String: Key {
-    var stringValue: String { self }
+    public var stringValue: String { self }
 }
 
 extension Int: Key {
-    var stringValue: String { "\(self)" }
+    public var stringValue: String { "\(self)" }
 }
 
 
-protocol Keyable {
+public protocol Keyable {
     associatedtype Keys: Key
     static var keys: [PartialKeyPath<Self>: Keys] { get }
 }
--- Sources/Predicates.swift/Converters/NSPredicateConvertible.swift
+++ Sources/Predicates.swift/Converters/NSPredicateConvertible.swift
@@ -7,71 +7,71 @@
 
 import Foundation
 
-protocol NSPredicateConvertible: Predicate where Root: Keyable {
+public protocol NSPredicateConvertible: Predicate where Root: Keyable {
     func nsPredicate() -> NSPredicate
 }
 
 
 //MARK: - Element Predicates
 extension Equals: NSPredicateConvertible where Root: Keyable {
-    func nsPredicate() -> NSPredicate {
+    public func nsPredicate() -> NSPredicate {
         return NSPredicate(format: "\(key()) = %@", argumentArray: [self.value])
     }
 }
 
 extension GreaterThan: NSPredicateConvertible where Root: Keyable {
-    func nsPredicate() -> NSPredicate {
+    public func nsPredicate() -> NSPredicate {
         return NSPredicate(format: "\(key()) > %@", argumentArray: [self.value])
     }
 }
 
 extension LessThan: NSPredicateConvertible where Root: Keyable {
-    func nsPredicate() -> NSPredicate {
+    public func nsPredicate() -> NSPredicate {
         return NSPredicate(format: "\(key()) < %@", argumentArray: [self.value])
     }
 }
 
 extension GreaterThanOrEqual: NSPredicateConvertible where Root: Keyable {
-    func nsPredicate() -> NSPredicate {
+    public func nsPredicate() -> NSPredicate {
         return NSPredicate(format: "\(key()) >= %@", argumentArray: [self.value])
     }
 }
 
 extension LessThanOrEqual: NSPredicateConvertible where Root: Keyable {
-    func nsPredicate() -> NSPredicate {
+    public func nsPredicate() -> NSPredicate {
         return NSPredicate(format: "\(key()) <= %@", argumentArray: [self.value])
     }
 }
 
 //MARK: - Bool Predicates
 extension BoolPredicate: NSPredicateConvertible where Root: Keyable {
-    func nsPredicate() -> NSPredicate {
+    public func nsPredicate() -> NSPredicate {
         return NSPredicate(value: self.value)
     }
 }
 
 //MARK: - Collection Predicates
 extension Contains: NSPredicateConvertible where Root: Keyable {
-    func nsPredicate() -> NSPredicate {
+    public func nsPredicate() -> NSPredicate {
         return NSPredicate(format: "%@ IN \(key())", argumentArray: [self.value])
     }
 }
 
 extension ContainsSet: NSPredicateConvertible where Root: Keyable {
-    func nsPredicate() -> NSPredicate {
+    public func nsPredicate() -> NSPredicate {
         return NSPredicate(format: "%@ IN \(key())", argumentArray: [self.value])
     }
 }
 
 //MARK: - Operators
 extension Not: NSPredicateConvertible where Root: Keyable, Base: NSPredicateConvertible {
-    func nsPredicate() -> NSPredicate {
+    public func nsPredicate() -> NSPredicate {
         return NSCompoundPredicate(notPredicateWithSubpredicate: self.basePredicate.nsPredicate())
     }
 }
 
 extension And: NSPredicateConvertible where LHS: NSPredicateConvertible, RHS: NSPredicateConvertible {
-    func nsPredicate() -> NSPredicate {
+    public func nsPredicate() -> NSPredicate {
         return NSCompoundPredicate(
             type: .and,
             subpredicates: [
@@ -82,7 +82,7 @@ extension And: NSPredicateConvertible where LHS: NSPredicateConvertible, RHS: NS
 }
 
 extension Or: NSPredicateConvertible where LHS: NSPredicateConvertible, RHS: NSPredicateConvertible {
-    func nsPredicate() -> NSPredicate {
+    public func nsPredicate() -> NSPredicate {
         return NSCompoundPredicate(
             type: .or,
             subpredicates: [
@@ -94,19 +94,19 @@ extension Or: NSPredicateConvertible where LHS: NSPredicateConvertible, RHS: NSP
 
 //MARK: - String Predicates
 extension StringContains: NSPredicateConvertible where Root: Keyable {
-    func nsPredicate() -> NSPredicate {
+    public func nsPredicate() -> NSPredicate {
         return NSPredicate(format: "\(key()) CONTAINS %@", argumentArray: [self.value])
     }
 }
 
 extension HasPrefix: NSPredicateConvertible where Root: Keyable {
-    func nsPredicate() -> NSPredicate {
+    public func nsPredicate() -> NSPredicate {
         return NSPredicate(format: "\(key()) BEGINSWITH %@", argumentArray: [self.value])
     }
 }
 
 extension HasSuffix: NSPredicateConvertible where Root: Keyable {
-    func nsPredicate() -> NSPredicate {
+    public func nsPredicate() -> NSPredicate {
         return NSPredicate(format: "\(key()) ENDSWITH %@", argumentArray: [self.value])
     }
 }
